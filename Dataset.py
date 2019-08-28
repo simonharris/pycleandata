b40cb95fd0cdd8e6862222e5318fdd3dd187c319
Simon H <github@pointbeing.net>
2019-08-28 16:15:54 +0100

    Print a little more info

--- Dataset.py
+++ Dataset.py
@@ -3,6 +3,7 @@ Main dataset processor
 """
 
 import os
+from pprint import pprint
 import urllib.request
 
 import numpy as np
@@ -20,6 +21,13 @@ class Dataset:
     def __init__(self, key, config):
         self._key = key
         self._config = config
+        
+        self._info = {
+            'url': self._config['data_url'],
+            'samples_pre': None,
+            'samples_post': None,
+            'labels': None,
+        }
         
     
     # Throws etc
@@ -41,8 +49,12 @@ class Dataset:
     def process(self): #, cleaner, labeller):
         """Data cleaning and label management"""  
         
+        self._info['samples_pre'] = self._ds.shape[0]
+        
         # Drop rows with missing data (TODO: make this more configurable?)
         self._ds = self._ds.dropna()
+  
+        self._info['samples_post'] = self._ds.shape[0]
   
         # Assume we always have labels...for now
         if 'label_col' in self._config:
@@ -51,6 +63,7 @@ class Dataset:
             lc = self._ds.shape[1]-1
         
         self._labels, _ = pd.factorize(self._ds.iloc[:,lc])
+        self._info['labels'] = len(np.unique(self._labels))
         self._ds = self._ds.drop(lc, axis=1)
 
         return True
@@ -62,6 +75,9 @@ class Dataset:
         # TODO: may need to handle other formats for output data     
         np.savetxt(location + '/labels.csv', self._labels, fmt=self.FORMAT_INT)
         np.savetxt(location + '/data.csv', self._ds, fmt=self.FORMAT_FLT, delimiter=',')
+        
+        pprint(self._info)
+        
         print("SAVED ALL TO:", location, "\n") 
 
        
